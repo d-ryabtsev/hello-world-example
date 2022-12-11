ce86038600e9b1826da752f2b2e9c516168566e8
Dmitry R <dmitry@reactive.works>
2022-12-11 02:07:58 +0000

    123

---
+++ src/styleguide/test-component.vue
@@ -0,0 +1,31 @@
+<template>
+<button class="test-component">
+  Click me
+</button>
+</template>
+
+<script>
+export default {
+  name: "test-component"
+}
+</script>
+
+<style>
+.test-component {
+  color: red
+}
+</style>
+
+<docs>
+	### Rationale
+
+	Some text goes here
+
+	### Usage
+
+  Some text goes here
+
+	### Examples
+
+  Some text goes here
+</docs>
